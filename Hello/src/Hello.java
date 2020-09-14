9e75ac61b751f426ee76b33d4bfaa0e5d47c03f2
Soham Bhowmick <sohamslc@gmail.com>
2020-09-14 12:21:52 +0530

    Added date in Hello class

--- Hello/src/Hello.java
+++ Hello/src/Hello.java
@@ -1,8 +1,12 @@
+import java.util.Date;
 
 public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Github");
+		
+		Date d=new Date();
+		System.out.println(d);
 	}
 
 }
