34fb740f795b7856a1123ac57391bc4986245e23
Soham Bhowmick <sohamslc@gmail.com>
2020-09-14 12:25:03 +0530

    Added LocalDateTime in Hello class

--- Hello/src/Hello.java
+++ Hello/src/Hello.java
@@ -1,8 +1,12 @@
+import java.time.LocalDateTime;
 
 public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Github");
+		
+		LocalDateTime dt=LocalDateTime.now();
+		System.out.println(dt);
 	}
 
 }
